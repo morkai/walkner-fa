e33b085abca96161448e7bd451d248a69afe1bd8
Łukasz Walukiewicz <lukasz@miracle.systems>
2019-11-17 21:36:01 +0100

    users: added a language preference.

--- bin/mongodb-update.js
+++ bin/mongodb-update.js
@@ -3,47 +3,6 @@
 
 'use strict';
 
-db.facostcenters.updateMany({owner: {$exists: false}}, {$set: {owner: null}});
+db.users.deleteMany({login: {$exists: false}});
 
-var stages = [
-  'protocol',
-  'verify',
-  'acceptOwner',
-  'acceptCommittee',
-  'acceptFinance',
-  'acceptDepartment',
-  'acceptDocument',
-  'record'
-];
-
-db.falts.find({}).forEach(lt =>
-{
-  var stageChangedAt = {};
-  var stageChangedBy = {};
-
-  stages.forEach(stage =>
-  {
-    stageChangedAt[stage] = lt.stageChangedAt[stage] || null;
-    stageChangedBy[stage] = lt.stageChangedBy[stage] || null;
-  });
-
-  if (!lt.committeeAcceptance)
-  {
-    lt.committeeAcceptance = {};
-
-    lt.committee.forEach(user =>
-    {
-      lt.committeeAcceptance[user._id] = {
-        time: new Date(),
-        user: user,
-        status: null
-      };
-    });
-  }
-
-  db.falts.updateOne({_id: lt._id}, {$set: {
-    stageChangedAt,
-    stageChangedBy,
-    committeeAcceptance: lt.committeeAcceptance
-  }});
-});
+db.users.updateMany({}, {$set: {preferences: {language: 'pl'}}});
