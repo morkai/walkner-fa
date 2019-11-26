5da7a1001e30c7dcbff9ef1c45538ba6116f6554
Łukasz Walukiewicz <lukasz@miracle.systems>
2019-11-26 19:12:28 +0100

    fa: removed month from the document numbers.

--- bin/mongodb-update.js
+++ bin/mongodb-update.js
@@ -4,3 +4,40 @@
 'use strict';
 
 db.faots.updateMany({photoFile: {$exists: false}}, {$set: {photoFile: null}});
+
+db.faots.find({}, {protocolNo: 1, documentNo: 1}).forEach(d =>
+{
+  db.faots.updateOne({_id: d._id}, fixNo(d));
+});
+
+db.falts.find({}, {protocolNo: 1, documentNo: 1}).forEach(d =>
+{
+  db.falts.updateOne({_id: d._id}, fixNo(d));
+});
+
+function fixNo(d)
+{
+  ['protocolNo', 'documentNo'].forEach(prop =>
+  {
+    var value = d[prop];
+
+    if (!value)
+    {
+      return;
+    }
+
+    var parts = value.split('/');
+
+    if (parts.length === 4)
+    {
+      parts[2] = null;
+    }
+
+    d[prop] = parts.filter(p => p !== null).join('/');
+  });
+
+  return {$set: {
+    protocolNo: d.protocolNo,
+    documentNo: d.documentNo
+  }};
+}
